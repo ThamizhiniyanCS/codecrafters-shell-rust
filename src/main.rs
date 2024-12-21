74528ac6ffdcd2eeaf4889a3efc71ddf6d26f0ce
Thamizhiniyan C S <csthamizhiniyan@gmail.com>
2024-12-22 00:01:25 +0530

    Run a program

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::path::Path;
-use std::process::exit;
+use std::process::{exit, Command};
 
-fn builtin_type(env_var_path: &String, command: &str) -> Option<String> {
+fn is_valid_executable_env_path(env_var_path: &String, command: &str) -> Option<String> {
     for p in env_var_path.split(":") {
         let path_string = format!("{p}/{command}").to_string();
         let path = Path::new(&path_string);
@@ -18,11 +18,65 @@ fn builtin_type(env_var_path: &String, command: &str) -> Option<String> {
     None
 }
 
-fn main() {
+fn custom_echo(args_string: Option<String>) {
+    if !args_string.is_none() {
+        println!("{}", args_string.unwrap());
+    }
+}
+
+fn custom_type(env_var_path: &String, args_string: Option<String>) {
     let commands: [&str; 3] = ["echo", "type", "exit"];
+
+    if !args_string.is_none() {
+        let arg = args_string.unwrap();
+
+        if commands.contains(&arg.as_str()) {
+            println!("{} is a shell builtin", arg);
+        } else {
+            let result: Option<String> = is_valid_executable_env_path(env_var_path, &arg);
+
+            if result.is_none() {
+                println!("{}: not found", arg);
+            } else {
+                println!("{} is {}", arg, result.unwrap());
+            }
+        }
+    }
+}
+
+fn custom_exit(args_string: Option<String>) {
+    if args_string.is_none() {
+        exit(0)
+    } else {
+        exit(args_string.unwrap().parse::<i32>().unwrap());
+    }
+}
+
+fn execute(env_var_path: &String, cmd: &str, args_string: Option<String>) {
+    let result: Option<String> = is_valid_executable_env_path(env_var_path, &cmd);
+
+    if result.is_none() {
+        println!("{}: not found", cmd);
+    } else {
+        let output = match args_string {
+            Some(args) => Command::new(cmd)
+                .args(args.split_whitespace())
+                .output()
+                .expect("Failed to execute process."),
+            None => Command::new(cmd)
+                .output()
+                .expect("Failed to execute process."),
+        };
+
+        let stdout = String::from_utf8(output.stdout).expect("Invalid utf-8 in process output");
+
+        println!("{}", stdout);
+    }
+}
+
+fn main() {
     let env_var_path = env::var("PATH").unwrap();
-    let regex_echo_pattern: Regex = Regex::new(r"^echo\s(.+)$").unwrap();
-    let regex_type_pattern: Regex = Regex::new(r"^type\s(.+)$").unwrap();
+    let regex_command_pattern: Regex = Regex::new(r"^(\w+)(?:\s(.+))?$").unwrap();
 
     loop {
         print!("$ ");
@@ -33,31 +87,37 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        input = input.trim().to_string();
+        let trimmed_input = input.trim().to_string();
 
-        let echo_capture: Option<regex::Captures<'_>> = regex_echo_pattern.captures(&input);
-        let type_capture: Option<regex::Captures<'_>> = regex_type_pattern.captures(&input);
+        let command_captures: Option<regex::Captures<'_>> =
+            regex_command_pattern.captures(&trimmed_input);
 
-        if input == "exit 0" {
-            exit(0);
-        } else if !echo_capture.is_none() {
-            println!("{}", echo_capture.unwrap().get(1).unwrap().as_str());
-        } else if !type_capture.is_none() {
-            let command: &str = type_capture.unwrap().get(1).unwrap().as_str();
+        let mut command: Option<&str> = Some("");
+        let mut args_string: Option<String> = Some(String::new());
 
-            if commands.contains(&command) {
-                println!("{} is a shell builtin", command);
+        if !command_captures.is_none() {
+            let capture = command_captures.unwrap();
+
+            command = if capture.get(1).is_none() {
+                None
             } else {
-                let result: Option<String> = builtin_type(&env_var_path, command);
+                Some(capture.get(1).unwrap().as_str())
+            };
 
-                if result.is_none() {
-                    println!("{}: not found", command);
-                } else {
-                    println!("{command} is {}", result.unwrap());
-                }
+            args_string = if capture.get(2).is_none() {
+                None
+            } else {
+                Some(capture.get(2).unwrap().as_str().to_string())
+            };
+        }
+
+        if !command.is_none() {
+            match command.unwrap() {
+                "echo" => custom_echo(args_string),
+                "type" => custom_type(&env_var_path, args_string),
+                "exit" => custom_exit(args_string),
+                _ => execute(&env_var_path, command.unwrap(), args_string),
             }
-        } else {
-            println!("{}: command not found", input);
         }
     }
 }
