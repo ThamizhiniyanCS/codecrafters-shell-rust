2fcef1511e155b99bcfa26f3fb827a1919ae1d01
Thamizhiniyan C S <csthamizhiniyan@gmail.com>
2024-12-21 21:48:00 +0530

    The echo builtin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use regex::Regex;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 use std::process::exit;
 
 fn main() {
+    let regex_echo_pattern: Regex = Regex::new(r"^echo\s(.+)$").unwrap();
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -12,10 +15,16 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        if input.trim() == "exit 0" {
-            exit(0);
-        }
+        input = input.trim().to_string();
 
-        println!("{}: command not found", input.trim());
+        let echo_capture = regex_echo_pattern.captures(&input);
+
+        if input == "exit 0" {
+            exit(0);
+        } else if !echo_capture.is_none() {
+            println!("{}", echo_capture.unwrap().get(1).unwrap().as_str());
+        } else {
+            println!("{}: command not found", input.trim());
+        }
     }
 }
