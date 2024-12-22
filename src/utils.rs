00ac112d15f3a00459d0d182ce85185c1d7bad2d
Thamizhiniyan C S <csthamizhiniyan@gmail.com>
2024-12-22 23:55:47 +0530

    Backslash outside quotes

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regex::{Regex, RegexSet};
 use std::env;
 use std::path::Path;
 
@@ -17,44 +17,146 @@ pub fn is_valid_executable_env_path(command: &str) -> Option<String> {
     None
 }
 
+fn is_separate_argument(
+    m: regex::Match<'_>,
+    characters: &Vec<char>,
+    characters_len: usize,
+) -> Option<String> {
+    let before_0: char = match m.start() == 0 {
+        true => ' ',
+        false => characters[m.start() - 1],
+    };
+    let before_1: char = match m.start() == 0 || m.start() == 1 {
+        true => ' ',
+        false => characters[m.start() - 2],
+    };
+    let after: char = match m.end() == characters_len {
+        true => ' ',
+        false => characters[m.end()],
+    };
+
+    match before_0 == ' ' && after == ' ' && before_1 != '\\' {
+        true => Some(m.as_str().to_string()),
+        false => None,
+    }
+}
+
 pub fn process_args(args_string: &String) -> Vec<String> {
-    let regex_single_quotes_pattern: Regex = Regex::new(
-        r#"'([^']*)'|"([^"]*)"|((?:\.\./|\./|/)?(?:[\w-]+/)*[\w-]+)|((?:\.\./|\./|\.)+)|(\w+)|(/)"#,
-    )
-    .unwrap();
-
-    let captures: regex::CaptureMatches<'_, '_> =
-        regex_single_quotes_pattern.captures_iter(&args_string);
-
+    let regex_expressions = [
+        // Capture strings within Single quotes
+        r#""([^"]*)""#,
+        // Capture strings within Double quotes
+        r#"'([^']*)'"#,
+        // Capture normal strings
+        r#"(\w+)"#,
+        // Capture normal and path strings without quotes and with backslashes
+        r#"(~?(?:(?:(?:\.\./|\./|/)?(?:\w+)(?:\.\./|\./|/))+)?(?:(?:\w+)?(?:\\.)+(?:\w+)?(?:\\.\w+)*/?))"#,
+        // Capture file paths without backslashes
+        r#"(~?(?:(?:\.\./|\./|/)?(?:[\w-]+)/?)+)+"#,
+        // Capture ./ and ../
+        r#"((?:\.\./|\./)+)"#,
+        // Capture ., ~ and /
+        r#"(\.|/|~)"#,
+    ];
+    let characters: Vec<char> = args_string.chars().collect();
+    let characters_len: usize = characters.len();
     let mut results: Vec<String> = Vec::new();
 
-    for capture in captures {
-        match capture.get(1) {
-            Some(c) => results.push(c.as_str().to_string()),
-            None => match capture.get(2) {
-                Some(c) => results.push(c.as_str().to_string()),
-                None => match capture.get(3) {
-                    Some(c) => results.push(c.as_str().to_string()),
-                    None => match capture.get(4) {
-                        Some(c) => results.push(c.as_str().to_string()),
-                        None => match capture.get(5) {
-                            Some(c) => results.push(c.as_str().to_string()),
-                            None => match capture.get(6) {
-                                Some(c) => results.push(c.as_str().to_string()),
-                                None => (),
-                            },
-                        },
-                    },
-                },
-            },
+    let regex_set = RegexSet::new(regex_expressions).unwrap();
+
+    let regexes: Vec<_> = regex_set
+        .patterns()
+        .iter()
+        .map(|pat| Regex::new(pat).unwrap())
+        .collect();
+
+    for index in regex_set.matches(&args_string).into_iter() {
+        match index {
+            // Capture strings within Single quotes
+            0 => {
+                for capture in regexes[0].captures_iter(&args_string) {
+                    let m = capture.get(1).unwrap().as_str().to_string();
+
+                    results.push(m)
+                }
+            }
+            // Capture strings within Double quotes
+            1 => {
+                for capture in regexes[1].captures_iter(&args_string) {
+                    let m = capture.get(1).unwrap().as_str().to_string();
+
+                    results.push(m)
+                }
+            }
+            // Capture normal strings
+            2 => {
+                for capture in regexes[2].captures_iter(&args_string) {
+                    match is_separate_argument(capture.get(1).unwrap(), &characters, characters_len)
+                    {
+                        Some(res) => results.push(res),
+                        None => (),
+                    }
+                }
+            }
+            // Capture normal and path strings without quotes and with backslashes
+            3 => {
+                for capture in regexes[3].captures_iter(&args_string) {
+                    match is_separate_argument(capture.get(1).unwrap(), &characters, characters_len)
+                    {
+                        Some(res) => {
+                            let mut temp = res;
+
+                            if !temp.contains('/') {
+                                temp = temp.replace('\\', "");
+                            };
+
+                            results.push(temp);
+                        }
+                        None => (),
+                    }
+                }
+            }
+            // Capture file paths without backslashes
+            4 => {
+                for capture in regexes[4].captures_iter(&args_string) {
+                    match is_separate_argument(capture.get(1).unwrap(), &characters, characters_len)
+                    {
+                        Some(res) => {
+                            if res.contains("/") {
+                                results.push(res)
+                            }
+                        }
+                        None => (),
+                    }
+                }
+            }
+            // Capture ./ and ../
+            5 => {
+                for capture in regexes[5].captures_iter(&args_string) {
+                    match is_separate_argument(capture.get(1).unwrap(), &characters, characters_len)
+                    {
+                        Some(res) => results.push(res),
+                        None => (),
+                    }
+                }
+            }
+            // Capture ., ~ and /
+            6 => {
+                for capture in regexes[6].captures_iter(&args_string) {
+                    match is_separate_argument(capture.get(1).unwrap(), &characters, characters_len)
+                    {
+                        Some(res) => results.push(res),
+                        None => (),
+                    }
+                }
+            }
+            _ => (),
         }
     }
 
     if results.is_empty() {
         results.push(args_string.to_string())
     }
-
-    // println!("{:#?}", results);
 
     results
 }
